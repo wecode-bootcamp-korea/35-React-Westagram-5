17191dd891aed739f008b774bb89b2f9b0806528
Soojung Shin <doublesj@Soojungui-MacBook-Pro.local>
2022-07-10 19:44:15 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/shinsoojung/Login/Login.js
+++ src/pages/shinsoojung/Login/Login.js
@@ -1,10 +1,88 @@
-import React from 'react';
-import "./Login.scss";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Login.scss';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/main-shinsoojung');
+  };
+
+  const [loginId, setLoginId] = useState('');
+  const [loginPW, setLoginPw] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  const handleIdInput = e => setLoginId(e.target.value);
+  const handlePwInput = e => setLoginPw(e.target.value);
+
+  const handleBtn = () => {
+    loginId.includes('@') && loginPW.length >= 5
+      ? setDisabled(false)
+      : setDisabled(true);
+  };
+  //  console.log(loginId);
+  //  console.log(loginPW);
   return (
     <div>
-      <h1>5Team</h1>
+      <main className="loginPage">
+        <div className="loginBox">
+          <div className="inputBox">
+            <h1 className="logo">westagram</h1>
+            <form className="inputIdPw">
+              <input
+                onChange={handleIdInput}
+                onKeyUp={handleBtn}
+                className="loginId"
+                type="id"
+                placeholder="전화번호, 사용자 이름 또는 이메일"
+              />
+              <input
+                onChange={handlePwInput}
+                onKeyUp={handleBtn}
+                className="loginPw"
+                type="password"
+                placeholder="비밀번호"
+              />
+              <button
+                onClick={goToMain}
+                className={disabled ? 'loginConfirm' : 'loginConfirm_pass'}
+                disabled={disabled}
+                type="submit"
+              >
+                로그인
+              </button>
+              <div className="orLine">
+                <div className="line" />
+                <div className="or">또는</div>
+                <div className="line" />
+              </div>
+              <div className="tryLoginFB">
+                <div className="facebookLogin">
+                  <img
+                    alt="facebookicon"
+                    className="facebookicon"
+                    src="images/shinsoojung/Login/fb_icon.png"
+                  />
+                  <div className="facebookLoginText">
+                    <a href="https://url.kr/j7pfcl">Facebook으로 로그인</a>
+                  </div>
+                </div>
+                <a
+                  className="findPw"
+                  href="https://www.instagram.com/accounts/password/reset/"
+                >
+                  비밀번호를 잊으셨나요?
+                </a>
+              </div>
+            </form>
+          </div>
+          <form className="joinMember">
+            <button className="goMembershipJoin">
+              계정이 없으신가요? <Link to="/main-shinsoojung">가입하기</Link>
+            </button>
+          </form>
+        </div>
+      </main>
     </div>
   );
 };
--- src/pages/shinsoojung/Main/Main.js
+++ src/pages/shinsoojung/Main/Main.js
@@ -1,10 +1,120 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import "./Main.scss";
 
 const Main = () => {
+
+  const navigate = useNavigate();
+  const goToLogin = () => {
+    navigate("/")
+  }
+
   return (
     <div>
-      <h1>5Team</h1>
+       <nav>
+      <div className="homeNavBox">
+        <button onClick={goToLogin}><h2 className="logoSmall">westagram</h2></button>
+        <div className="searchBar">
+          <img className="searchIcon" src="images/shinsoojung/Main/search.png" alt="searchIcon" />
+          <input className="inputSearch" type="text" placeholder="검색" />
+        </div>
+        <div className="navIcon">
+          <div><img alt="newFeedIcon" src="images/shinsoojung/Main/newFeed.jpg" /></div>
+          <div><img alt="homeIcon" src="images/shinsoojung/Main/home.jpg" /></div>
+          <div><img alt="dmIcon" src="images/shinsoojung/Main/dm.jpg" /></div>
+          <div><img alt="exploreIcon" src="images/shinsoojung/Main/explore.jpg" /></div>
+          <div><img alt="heartIcon" src="images/shinsoojung/Main/heart.jpg" /></div>
+          <div><img alt="myProfileIcon" src="images/shinsoojung/Main/myprofile.png" id='navProfileIcon' /></div>
+        </div>
+      </div>
+    </nav>
+    <main>
+      <section className="feedScrollArea">
+        <article className="feed">
+          <div className="feedNav">
+            <div className="feedNavIconName">
+              <img id="feedProfileIcon" src="images/shinsoojung/Main/myprofile.png" alt="myProfile" />
+              <div className="feedProfileName">doublejs1226</div>
+            </div>
+            <img className="feedPlusIcon" src="images/shinsoojung/Main/dot.png" alt="feedNavDot" />
+          </div>
+          <div className="feedImageSection">
+            <img src="images/shinsoojung/Main/feed_img1.jpg" alt="feedimg1" id="feedImg" />
+          </div>
+          <div className="feedImageResponse">
+            <div className="feedImageResponseInner">
+              <img src="images/shinsoojung/Main/heartbeat.jpg" alt="heartBeatIcon" />
+              <img src="images/shinsoojung/Main/comment.jpg" alt="commentIcon" />
+              <img src="images/shinsoojung/Main/dm.jpg" alt="dmIcon" />
+            </div>
+            <img src="images/shinsoojung/Main/cliping.jpg" alt="clipingIcon" />
+          </div>
+          <article className="commentArea">
+            <div className="heartBeatPeople">
+              <p><span className="userName">doublejs1226</span>님 <span className="userName">여러 명</span>이 좋아합니다.</p>
+            </div>
+            <div className="newComment">
+              {/* 신규 코멘트 영역 */}
+            </div>
+          </article>
+          <div className="commentBox">
+            <form className="inputCommentsBox">
+              <input id="commentInput" type="text" placeholder="댓글 달기..." />
+              <button id="commentSubmit">게시</button>
+            </form>
+          </div>
+        </article>
+      </section>
+      <aside className='stickyAsideArea'>
+        <ul>
+          <li className="asideMyProfileBox">
+            <div className="asideMyProfile">
+              <div className="asideMyProfileImg">
+                <img src="images/shinsoojung/Main/myprofile.png" alt="myProfile"/>
+              </div>
+              <div className="asideFeedMyProfileName">doublejs1226 <div className="asideFeedMyProfileSubName">수정</div>
+              </div>
+            </div>
+            <div className="asideMyIdRetrun" onClick={goToLogin}>전환</div>
+          </li>
+          <li className="recommandListTitle">
+            <div>회원님을 위한 추천</div>
+            <div>모두 보기</div>
+          </li>
+          <li className="recommandPeopleBox">
+              <div className="recommandPeopleList">
+                <div className="recommendProfileIcon">
+                  <img src="images/shinsoojung/Main/myprofile.png" alt="myProfile"/>
+                </div>
+                <div className="recommendProfileName">doublejs1226<div className="recommendProfileSubName">수정</div>
+                </div>
+              </div>
+              <div className="recommendPeopleFollow">팔로잉</div>
+          </li>
+          <li className="recommandPeopleBox">
+              <div className="recommandPeopleList">
+                <div className="recommendProfileIcon">
+                  <img src="images/shinsoojung/Main/myprofile.png" alt="myProfile"/>
+                </div>
+                <div className="recommendProfileName">doublejs1226<div className="recommendProfileSubName">수정</div>
+                </div>
+              </div>
+              <div className="recommendPeopleFollow">팔로잉</div>
+          </li>
+          <li className="recommandPeopleBox">
+              <div className="recommandPeopleList">
+                <div className="recommendProfileIcon">
+                  <img src="../../images/myprofile.png" alt="myProfile"/>
+                </div>
+                <div className="recommendProfileName">doublejs1226<div className="recommendProfileSubName">수정</div>
+                </div>
+              </div>
+              <div className="recommendPeopleFollow">팔로잉</div>
+          </li>
+        </ul>
+      </aside>
+    </main>
     </div>
   );
 };
